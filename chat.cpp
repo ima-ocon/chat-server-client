619044e30ed1e76d6531edb7c6a2739b328033df
ima-ocon <ima_ocon@yahoo.com>
2016-04-29 09:37:21 +0800

    Added @exit

--- chat.cpp
+++ chat.cpp
@@ -15,21 +15,31 @@ void error(const char *msg)
 
 int main(int argc, char *argv[])
 {
+  //the name of the person
   int name_size = 1000;
   char name[name_size];
+  //the name of the chatmate
   char name_chatmate[name_size];
+
+  char socket_check[2];
 
   int sockfd, newsockfd, portno;
   socklen_t clilen;
 
+  //buffer -> can contain either what the person sends/receives
   int buffer_size = 8;
   char buffer[buffer_size];
+
+  //only for transfer purposes
   char bufferTemp[buffer_size];
 
   struct sockaddr_in serv_addr, cli_addr;
   //n -> num of characters read/written
   int n;
 
+  //server/client connects
+
+  //if server
   if (argc == 3) {
     strncpy(name, argv[2], 1000);
 
@@ -58,6 +68,7 @@ int main(int argc, char *argv[])
     if (newsockfd < 0)
          error("ERROR on accept");
   }
+  //if client
   else if (argc == 4) {
       strncpy(name, argv[3], 1000);
 
@@ -86,45 +97,72 @@ int main(int argc, char *argv[])
      exit(0);
   }
 
+  //server/client receives message
+
+//technically a boolean
+//
     int isEndOfMessage = 0;
 
     send(newsockfd, name, name_size - 1, 0);
 
     recv(newsockfd, name_chatmate, name_size-1, 0);
 
+    int x = 0;
+
     while (true) {
       printf("%s: ", name);
       bzero(buffer, buffer_size);
       fgets(buffer, buffer_size-1,stdin);
 
-      if (strcmp(buffer, "@exit\n") == 0)
-        exit(0);
-
+      //hasNextMessage is a boolean (int because C has no booleans)
+      //used when sending
+      //refer to the sending conditional for more info
       int hasNextMessage = 0;
+      //if the user presses enter, receive messages
       if (buffer[0] == '\n') {
+        //n -> number of bytes received
+        //Usually recv "blocks"--waits indefinitely for an input
+        //But with the flag MSG_DONTWAIT, it checks right away if there's
+        //an input, assigns 0 if none, then moves on
         n = recv(newsockfd, buffer, buffer_size-1, MSG_DONTWAIT);
-        if (n <= 0)
+        //n < 0 if there's no new response from the other side
+        if (n < 0)
           printf("%s:\n", name_chatmate);
+        //if there are messages from the other person
+        else if (n == 0) {
+          close(newsockfd);
+          if (argc == 2)
+            close(sockfd);
+          exit(1);
+        }
         else {
           printf("%s: ", name_chatmate);
           do {
-//            printf("%s", buffer);
+            //assigns the received message to bufferTemp
             n = recv(newsockfd, bufferTemp, buffer_size-1, MSG_DONTWAIT);
 
-//            printf("\nbreak\n");
+            if (strcmp(buffer, "@exit\n") == 0) {
+              close(newsockfd);
+              if (argc == 2)
+                close(sockfd);
+              exit(0);
+            }
+
+            //just puts the name of the chatmate in front of each new line
             for (int x = 0; x < buffer_size-1; x++) {
               printf("%c", buffer[x]);
               if (buffer[x] == '\n' && n > 0)
                 printf("%s: ", name_chatmate);
             }
 
+            //puts bufferTemp in buffer
             strncpy(buffer, bufferTemp, buffer_size);
-//            buffer = bufferTemp;
           } while (n > 0);
         }
       } else {
         while (true) {
-//          printf("sending: %s\n", buffer);
+          //already takes care of overflow
+          //keeps on sending until input is consumed
           send(newsockfd, buffer, buffer_size - 1, 0);
 
           for (int x = 0; x < buffer_size; x++) {
@@ -136,6 +174,13 @@ int main(int argc, char *argv[])
             break;
 
           fgets(buffer, buffer_size, stdin);
+        }
+
+        if (strcmp(buffer, "@exit\n") == 0) {
+          close(newsockfd);
+          if (argc == 2)
+            close(sockfd);
+          exit(0);
         }
 
         isEndOfMessage = 0;
